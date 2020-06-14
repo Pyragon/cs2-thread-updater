c18907ca111bb97797117170390674c9f106fe65
Cody Thompson <eldo.imo.rs@hotmail.com>
2020-06-14 00:38:21 -0700

    Add webhook listening

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const puppeteer = require('puppeteer');
 const props = require('./props.json');
 
-async function run() {
+const { Webhooks } = require("@octokit/webhooks");
+const webhooks = new Webhooks({
+  secret: props['github_secret'],
+});
+
+async function updateThread(content) {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -37,7 +42,7 @@ async function run() {
 
     let text = await page.evaluate(e => e.value, editForm);
 
-    await page.evaluate(() => {
+    await page.evaluate((content) => {
 
         String.prototype.splice = function(idx, rem, str) {
             return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
@@ -45,8 +50,8 @@ async function run() {
 
         let form = document.querySelector('td>textarea');
         let index = form.value.indexOf('ater[/url]') + 11;
-        form.value = form.value.splice(index, 0, "\ntesting auto-thread updater");
-    });
+        form.value = form.value.splice(index, 0, "\n"+content);
+    }, content);
 
     await delay(5000);
 
@@ -67,4 +72,18 @@ function delay(time) {
     });
  }
 
-run();
+ webhooks.on("*", ({ id, name, payload }) => {
+    if(name == 'push') {
+        let results = '';
+        for(let i = 0; i < payload.commits.length; i++) {
+            let commit = payload.commits[i];
+            results += payload.repository.name+': [URL="'+commit.url+'"]'+commit.message+'[/URL]';
+            if(i != payload.commits.length-1)
+                results += '\n';
+        }
+        updateThread(results);
+    }
+ });
+ 
+ require("http").createServer(webhooks.middleware).listen(3000);
+ // can now receive webhook events at port 3000
